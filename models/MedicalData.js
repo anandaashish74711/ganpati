7a3aa3812d6826f4e7c25764fadaf11b49f64736
kartik satish devarde <kartikdevarde@outlook.com>
2023-11-15 18:21:15 +0530

    fix error

--- models/MedicalData.js
+++ models/MedicalData.js
@@ -1,54 +1,59 @@
-
 const mongoose = require('mongoose');
-const { Timeseries } = require('mongoose-timeseries');
 
-const ObservationSchema = new mongoose.Schema({
-  observationNumber: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  timestamp: {
-    type: Date,
-    required: true,
-    index: true, // Add an index for efficient time-based queries
-  },
-  frequency: {
-    type: Number,
-    required: true,
-  },
-  postGenerator: {
-    type: Number,
-    required: true,
-  },
-  postSensor: {
-    type: Number,
-    required: true,
-  },
-  bioImpedance: {
-    type: Number,
-    required: true,
-  },
-  phaseAngle: {
-    type: Number,
-    required: true,
-  },
-  stepSize: {
-    type: Number,
-    required: true,
-  },
-  numberOfPoints: {
-    type: Number,
-    required: true,
-  },
-});
+const ObservationSchema =  new mongoose.Schema(
+    {
+      timestamp: Date,
+      messages: Number,
+      
+      observationNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+      },
+      frequency: {
+        type: Number,
+        required: true,
+      },
+      postGenerator: {
+        type: Number,
+        required: true,
+      },
+      postSensor: {
+        type: Number,
+        required: true,
+      },
+      bioImpedance: {
+        type: Number,
+        required: true,
+      },
+      phaseAngle: {
+        type: Number,
+        required: true,
+      },
+      stepSize: {
+        type: Number,
+        required: true,
+      },
+      numberOfPoints: {
+        type: Number,
+        required: true,
+      },
 
-// Configure the Mongoose time series plugin
-ObservationSchema.plugin(Timeseries, {
-  timeField: 'timestamp', 
-  granularity: 'second', 
-  metaField: 'meta', 
-});
+      metadata: {
+        discordId: String,
+      },
+    },
+    {
+      timeseries: {
+        timeField: 'timestamp',
+        metaField: 'metadata',
+        granularity: 'hours',
+      },
+    },
+  );
+
+
 
 const Observation = mongoose.model('Observation', ObservationSchema);
 
+module.exports = Observation;
--- routes/medicaldata.router.js
+++ routes/medicaldata.router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { addMedicalData } = require('../controllers/medicaldata')    
+const addObservationToVisit = require('../controllers/medicaldata')
 
 
 router.post('/observation', addObservationToVisit);
