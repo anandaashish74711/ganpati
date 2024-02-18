2b1bae21cc8d261107a4811b2d9aa39f4f602fb5
Aashish Anand <anandaashish512@gmail.com>
2024-02-19 01:58:29 +0530

    cors-error-resolve

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
@@ -14,7 +15,7 @@ const user = require('./routes/user');
 const medicaldata = require('./routes/medicaldata.router');
 const patient = require('./routes/patient.routes');
 const Nurse=require('./routes/nurse.routes')
-
+app.use(cors())
 app.use('api/v1',auth);
 app.use('/api/v1', user);
 app.use('/api/v1', medicaldata);
